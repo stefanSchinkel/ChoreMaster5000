979325f61260938ee7038dfcf3639bee9a57f2c9
Stefan Schinkel <mail@dreeg.org>
2019-02-21 21:37:17 +0100

    add schema

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS chores (
+    chore_id integer PRIMARY KEY AUTOINCREMENT,
+    description text NOT NULL,
+    multiplier integer,
+    target integer
+);
+
+CREATE TABLE IF NOT EXISTS log (
+    day date,
+    chore_id text NOT NULL,
+    counter integer,
+    PRIMARY KEY (day, chore_id),
+    FOREIGN KEY(chore_id) REFERENCES chores(chore_id)
+    
+);
