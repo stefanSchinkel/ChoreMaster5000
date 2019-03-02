8c0f8174a62e8d6c6aeafc9ebced5cb206a02483
Stefan Schinkel <mail@dreeg.org>
2019-03-02 12:45:41 +0100

    update wrapper

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from choremaster import create_app
 from choremaster.config import config
 
 if __name__ == '__main__':
-    app = create_app(config)
+    app = create_app()
     app.run(host='0.0.0.0', port="8888", debug=True)
