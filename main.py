d6d2ae4b8ccc9ab7cba5c543ea72f36a4db27770
Stefan Schinkel <mail@dreeg.org>
2019-03-04 22:28:47 +0100

    update port in wrapper

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from choremaster import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port="8888", debug=True)
+    app.run(host='0.0.0.0', port=7777, debug=True)
