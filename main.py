ab07926e0d5bca6fa35ed9903e28866a44bd06fe
Stefan Schinkel <mail@dreeg.org>
2024-07-08 18:15:37 +0200

    noexec on main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
 from choremaster import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=7777, debug=True)
+    app.run(host='0.0.0.0', port=7777, debug=False)
